40695237832a26078b003a8b9d240c6e1c7894b1
Segge <seggbenga@gmail.com>
2022-09-08 22:15:31 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 
 /**
 * The main function prints the strings in the puts function
-* This program prints "Programming is like building a multilingual puzzle
-*Return 0 i.e it ends with value 0
+*
+*Description: Using the main function
+*This program prints "Programming is like building a multilingual puzzle
+*Return 0 
 */
 int main(void)
 {
