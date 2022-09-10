c61e897965beade3c0fc3e45f282d09f656c7e3b
Segge <seggbenga@gmail.com>
2022-09-10 15:17:25 -0700

    prints number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d ", n);
+printf("%i ", n);
 printf("is positive");
 }
 else if (n < 0)
 {
-printf("%d ", n);
+printf("%i ", n);
 printf("is negative");
 }
 else
 {
-printf("%d ", n);
+printf("%i ", n);
 printf("is zero");
 }
 return (0);
