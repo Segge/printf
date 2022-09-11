ce6605414ca64ca2e0ac70854b306a473ec9c1e0
segge <seggbenga@gmail.com>
2022-09-11 23:57:12 +0100

    prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *Decription:main-prints all different combinations of three digits.
+ *Return: 0 if success
+ *
+ */
+int main(void)
+{
+	int i, j, k;
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			for (k = j; k < 58; k++)
+			{
+				if (k == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(K);
+				if (i == 55 && j == 56 && k = 57)
+				{
+					break;
+				}
+putchar(',');
+putchar(' ');
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
