e71b8c1a7238cce57249e6acb4a74d261ed47c13
segge <seggbenga@gmail.com>
2022-09-15 23:35:26 +0100

    prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 
 /**
  *more_numbers-prints numbers 0 to 14 ten times
- *Return: returns nothing
+ *Return:returns nothing
  */
 
 void more_numbers(void)
@@ -13,13 +13,13 @@ void more_numbers(void)
 	for (n0 = 0; n0 < 10; n0++)
 	{
 		for (n1 = 0; n1 <= 14; n1++)
-		{	
+		{
 			if (n1 > 9)
 			{
 				putchar((n1 / 10) + '0');
 			}
 			putchar((n1 % 10) + '0');
-		}	
+		}
 
 		putchar(10);
 	}
