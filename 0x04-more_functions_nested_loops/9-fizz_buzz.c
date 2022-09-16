27df4bc42b2d36788ebc2dd5ca6e6371b2415798
Segge <seggbenga@gmail.com>
2022-09-16 16:53:07 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,22 @@ int main(void)
 	{
 		if (a % 15 == 0)
 		{
-		printf("FizzBuzz");
-		printf(" ");
+		printf("FizzBuzz ");
 		}
 		else if (a % 3 == 0)
 		{
-		printf("Fizz");
-		printf(" ");
+		printf("Fizz ");
 		}
 		else if (a % 5 == 0)
 		{
-		printf("Buzz");
-		printf(" ");
+		printf("Buzz ");
 		}
 		else
 		{
-		printf("%i", a);
+		printf("%i ", a);
 			if (a < 100)
 			{
-			printf(" ");
+			 printf(" ");
 			}
 		}
 	}
