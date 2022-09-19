4e3a540083295a33304b4875f4d9d029e42b7638
Segge <seggbenga@gmail.com>
2022-09-19 23:00:25 +0100

    new fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,15 +12,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		i++;
 		if ((i % 2) == 0)
-		
 			_putchar(str[i]);
 		else
 			continue;
-
 	}
 	 _putchar('\n');
 }
