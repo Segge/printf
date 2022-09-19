e60bd5ab3480f885868e6d1cde6b96994dcb9ba6
Segge <seggbenga@gmail.com>
2022-09-19 15:58:00 +0100

    Pointer to update value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - points to an integer and updates the value to 98
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n = 402;
+	*p = &n;
+
+	*p = 98;
+
+	printf("n = %d\n", &n);
+
+	return (0);
+}
+
